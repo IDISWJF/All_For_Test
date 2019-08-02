12b02630e57d2dfcfd21b8487053e8bad31e3d7e
jfwang <jfwang@nullmax.ai>
2019-08-02 17:08:06 +0800

    add client and test exlpan

--- server.cpp
+++ server.cpp
@@ -14,18 +14,17 @@ void read_cb(struct bufferevent* bev, void* arg)
     bufferevent_read(bev, buf, sizeof(buf));
     std::cout<<"read buff: "<<buf;
     //发送数据--往缓冲区中写数据
-    const char* ptr = "read cb : already read !\n";
+    const char* ptr = "read cb : already read !";
     //会在常量区定义一个string类型的常量，并返回一个常量指针指向字符串
     //常量指针是const类型的，所以必须使用const类型指针（c中可以使用非const指针）
-    bufferevent_write(bev, ptr, strlen(ptr));
+    bufferevent_write(bev, ptr, strlen(ptr));//写入时会自动发送，默认函数调用后在调用w_cb主要是为了进行通知
     //ptr是指针4字节,64位系统8字节,如果使用sizeof只能返回8字节（read cb ）
     //buf虽然是数组首元素地址，但是数组可以理解为buf+1024（数组长度），所以sizeof的结果是1024
-    std::cout<<"already write for client !"<<std::endl;
 }
 //写回调
 void write_cb(struct bufferevent* bev, void* arg)
 {
-    std::cout<<"write_cb : already write !"<<std::endl;
+    //std::cout<<"write_cb : already write !"<<std::endl;
 }
 //事件回调
 void event_cb(struct bufferevent* bev, short events, void* arg)
@@ -49,9 +48,9 @@ void listen_cb(struct  evconnlistener* listen,
     //得到传进来的event_base
     struct event_base* base = (struct event_base*)ptr;
     //先接收数据，在发送数据
-    //将fd封装为bufferevent
+    //将fd封装为bufferevent事件中
     struct  bufferevent* bev = NULL;
-    bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);
+    bev = bufferevent_socket_new(base, fd, BEV_OPT_CLOSE_ON_FREE);//ON_FREE 不能释放事件而是释放事假所对应的文件描述符资源
     //给bufferevent读写缓冲区设置相应的回调函数
     bufferevent_setcb(bev, read_cb, write_cb, event_cb, NULL);
     //设置读缓冲可用，写缓冲默认是可用的
@@ -65,7 +64,7 @@ int main()
     struct sockaddr_in serv;
     memset(&serv, 0, sizeof(serv));
     serv.sin_family = AF_INET;
-    serv.sin_port = htons(80);
+    serv.sin_port = htons(1234);
     serv.sin_addr.s_addr = htonl(INADDR_ANY);
     //创建监听套接字
     //绑定
